9cc7f668d6fcbd675d32f0dde36f313dac3b6e7a
Aakash Rana <aakashfoss@gmail.com>
2017-02-13 14:47:18 +0530

    registed FieldOrder model in admin

--- tnp/consent/admin.py
+++ tnp/consent/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from consent.models import PersonalDetail, EducationDetail, CGPA, \
-UserConsent, UserDataFields, ConsentDeadline
+UserConsent, UserDataFields, ConsentDeadline, FieldOrder
 
 admin.site.register(PersonalDetail)
 admin.site.register(EducationDetail)
@@ -9,3 +9,4 @@ admin.site.register(CGPA)
 admin.site.register(UserConsent)
 admin.site.register(UserDataFields)
 admin.site.register(ConsentDeadline)
+admin.site.register(FieldOrder)
