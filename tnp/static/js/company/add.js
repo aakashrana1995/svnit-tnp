8a8509cd797b560a5fa9701e16fd2b41480fcef9
Aakash Rana <aakashfoss@gmail.com>
2017-02-13 14:48:45 +0530

    many frontend changes

--- tnp/static/js/company/add.js
+++ tnp/static/js/company/add.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
 	$('textarea').addClass('materialize-textarea');
-	
 	$('#down_left').css('margin-top', '75px');
 	
+    $('select[required]').removeAttr('required');
+
 	crpdate = $('#id_company_form-crpdate')[0];
 	crpdate.type = "date";
 	crpdate.className = "datepicker";
+  
+    deadline_date = $('#id_consent_deadline_form-deadline_date')[0];
+    deadline_date.type = "date";
+    deadline_date.className = "datepicker";  
 
-	deadline = $('#id_consent_deadline_form-deadline')[0];
-	deadline.type = "date";
-	deadline.className = "datepicker";
+    deadline_time = $('#id_consent_deadline_form-deadline_time')[0];
+    deadline_time.type = "time";
+    deadline_time.className = "timepicker";  
 
   	$('.datepicker').pickadate({
     	selectMonths: true, // Creates a dropdown to control month
-    	selectYears: 5 // Creates a dropdown of 5 years to control year
+    	selectYears: 5, // Creates a dropdown of 5 years to control year
+        //format: 'yyyy-mm-dd',
   	});
+
+    $('.timepicker').pickatime({
+    autoclose: false,
+    twelvehour: true,
+    default: 'now',
+  });
     
 
 	elems = document.getElementsByTagName('textarea');
@@ -22,74 +34,74 @@ $(document).ready(function() {
 		elems[i].style.height = "70px";
 	}
 	
-  $('select').material_select();
+    $('select').material_select();
 
-  $('#cgpa_upto_semester')[0].innerHTML = 'CGPA Upto Semester <div class="input-field inline"> <input type="number" value="6" style="width: 30px; text-align: center;"> </div>';
-  $('#cgpa_of_semester')[0].innerHTML = 'CGPA Of Semester <div class="input-field inline"> <input type="number" value="6" style="width: 30px; text-align: center;"> </div>';
-
+    $('#cgpa_upto_semester')[0].innerHTML = 'CGPA Upto Semester <input type="hidden" value="cgpa_upto_semester" name="A"> <div class="input-field inline"> <input type="number" value="6" name="A" style="width: 30px; text-align: center;"> </div>';
+    $('#cgpa_of_semester')[0].innerHTML = 'CGPA Of Semester <input type="hidden" value="cgpa_of_semester" name="C"> <div class="input-field inline"> <input type="number" value="6" name="C" style="width: 30px; text-align: center;"> </div>';
 });
 
 
- $('#add_more').click(function() {
+$('#add_more').click(function() {
     var form_idx = $('#id_job_location-TOTAL_FORMS').val();
     $('#job_location_formset').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
     $('#id_job_location-TOTAL_FORMS').val(parseInt(form_idx) + 1);
 });
 
- $('#job_location_formset').on("click", ".remove", function(e) {
-	var elem = e.target.closest('.element');
-	elem.remove();
+$('#job_location_formset').on("click", ".remove", function(e) {
+  var elem = e.target.closest('.element');
+  elem.remove();
 
-	var form_idx = $('#id_job_location-TOTAL_FORMS').val();
-	$('#id_job_location-TOTAL_FORMS').val(parseInt(form_idx) - 1);
+  var form_idx = $('#id_job_location-TOTAL_FORMS').val();
+  $('#id_job_location-TOTAL_FORMS').val(parseInt(form_idx) - 1);
 });
 
 
-$('#attachment_formset').on("click", ".delete_file", function(e) {
-	var elem = e.target.closest('.element');
-	elem.remove();
-    
-    var form_idx = $('#id_attachment-TOTAL_FORMS').val();
-    $('#id_attachment-TOTAL_FORMS').val(parseInt(form_idx) - 1);
-});
+$('#id_attachment_form-file').on("change", function(e) {
+    files_data = e.target.files;
+    if(files_data.length>1)
+        string = files_data.length + " files";
+    else
+        string = files_data[0].name;
 
-
-$('#add_more_files').click(function() {
-    var form_idx = $('#id_attachment-TOTAL_FORMS').val();
-    $('#attachment_formset').append($('#empty_form_attachment').html().replace(/__prefix__/g, form_idx));
-    $('#id_attachment-TOTAL_FORMS').val(parseInt(form_idx) + 1);
-});
-
-
-$('#attachment_formset').on("change", ".filename", function(e) {
-	filepath = e.target.value;
-    arr = filepath.split('\\');
-    filename = arr[arr.length-1];
-    
     var parent = e.target.closest('.file-field');
-    console.log(parent);
-    parent.getElementsByTagName('span')[1].innerHTML = filename;
+    parent.getElementsByTagName('span')[1].innerHTML = string;
 });
 
-$('#timepicker').pickatime({
-    autoclose: true,
-    twelvehour: true,
-    default: 'now'
-  });
 
 
 Sortable.create(A, {
   group: "sorting",
-  sort: true
+  sort: true,
+  onAdd: function (evt) {
+        var itemEl = evt.item;
+        elems = itemEl.getElementsByTagName('input');
+        for (var i=0; i<elems.length; i++) {
+            elems[i].name = "A";
+        }
+    },
 });
 
-// sort: false
 Sortable.create(B, {
   group: "sorting",
-  sort: true
+  sort: true,
+  onAdd: function (evt) {
+        var itemEl = evt.item;
+        elems = itemEl.getElementsByTagName('input');
+        for (var i=0; i<elems.length; i++) {
+            elems[i].name = "B";
+        }
+    },
 });
 
 Sortable.create(C, {
   group: "sorting",
-  sort: true
+  sort: true,
+  onAdd: function (evt) {
+        var itemEl = evt.item;
+        elems = itemEl.getElementsByTagName('input');
+        for (var i=0; i<elems.length; i++) {
+            elems[i].name = "A";
+        }
+    },
 });
+
