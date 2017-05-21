12b8cb14bad58f248410ab19b0c250fe32892566
Aakash Rana <aakashfoss@gmail.com>
2017-05-21 16:00:29 +0530

    change password page UI improvements

--- tnp/consent/views.py
+++ tnp/consent/views.py
@@ -601,11 +601,8 @@ def change_password(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-            messages.success(
-                request, 'Your password was successfully updated!')
+            messages.success(request, 'Your password was successfully updated!')
             return HttpResponseRedirect('/consent/home')
-        else:
-            messages.error(request, 'Please correct the error below.')
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'consent/change_password.html', {
