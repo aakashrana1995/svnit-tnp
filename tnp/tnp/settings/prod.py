706a6885f6fdb1e5aa7cf335db45f14a4e0bb097
Aakash Rana <aakashfoss@gmail.com>
2017-05-27 19:17:23 +0530

    minor changes in prod/stage settings file

--- tnp/tnp/settings/prod.py
+++ tnp/tnp/settings/prod.py
@@ -12,9 +12,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': 3306,
     }
 }
